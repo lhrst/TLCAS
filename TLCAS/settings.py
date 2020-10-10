41f32929054b3e36ae46ae0eab6fac38a02039c2
Herixth <17361019875@163.com>
2020-10-10 11:46:40 +0800

    create user model, make a backup and flush your local sql if migration failed

--- TLCAS/settings.py
+++ TLCAS/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'userinfo.UserInformation'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
