46cca7089e561e39660f70fa4d9af15a438f2858
Herixth <17361019875@163.com>
2020-10-10 21:03:09 +0800

    登陆功能实现

--- userinfo/views.py
+++ userinfo/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.conf import settings
 from django.core.validators import validate_email
+from django.contrib.auth import login
 from django.contrib.auth.hashers import make_password
 
 from userinfo import models as user_models
@@ -23,6 +24,8 @@ def verify_account(username, email, password1, password2, hashkey, captcha) -> s
         return '密码的长度不应小于8，大于128'
     if user_models.UserInformation.objects.filter(username=username, is_active=True):
         return '用户名[{}]已被注册'.format(username)
+    if '@' in username:
+        return '用户名中不能包含@'
     if user_models.UserInformation.objects.filter(email=email, is_active=True):
         return '邮箱[{}]已被注册'.format(email)
     return ''
@@ -50,14 +53,48 @@ def register(request):
             # send email
             code = email_views.make_confirm_string(newuser)
             email_views.send_email(rq_email, code)
-            return redirect("/")
+            return redirect("/login/")
         else:
             newcaptcha = captcha_views.generate_captcha()
             return render(request, "userinfo/register.html", {"captcha": newcaptcha, "message": message})
 
 
 def login(request):
-    return HttpResponse('login')
+    if request.method == "GET":
+        newcaptcha = captcha_views.generate_captcha()
+        return render(request, "userinfo/login.html", {"captcha": newcaptcha})
+    elif request.method == "POST":
+        rq_hashkey = request.POST.get('captcha_0', '')
+        rq_captcha = request.POST.get('captcha_1', '')
+        rq_user_or_email = request.POST.get('name_or_email', '').strip()
+        rq_password = request.POST.get('password', '').strip()
+        message = ''
+        if not captcha_views.verify_captcha(rq_captcha, rq_hashkey):
+            message = '验证码错误'
+        else:
+            if '@' in rq_user_or_email:
+                try:
+                    user = user_models.UserInformation.objects.get(email=rq_user_or_email, is_active=True)
+                    if not user.check_password(rq_password):
+                        message = '密码错误'
+                except:
+                    message = '邮箱不存在，请前往注册或进行邮箱认证'
+            else:
+                try:
+                    user = user_models.UserInformation.objects.get(username=rq_user_or_email, is_active=True)
+                    if not user.check_password(rq_password):
+                        message = '密码错误'
+                except:
+                    message = '用户名不存在，请前往注册或进行邮箱认证'
+        if message == '':
+            request.session['is_login'] = True
+            request.session['user_id'] = user.id
+            request.session['user_name'] = user.username
+            return redirect('/')
+        else:
+            newcaptcha = captcha_views.generate_captcha()
+            return render(request, "userinfo/login.html", {"captcha": newcaptcha, 'message': message})
+
 
 def logout(request):
     return HttpResponse('logout')
