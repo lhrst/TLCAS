bf54f26fa62cdecfccc3ca56d633a816ac7e226d
lhrst <litianqi@litianqideMacBook-Pro.local>
2020-10-12 16:48:15 +0800

    reset search and index page, need for design

--- papers/urls.py
+++ papers/urls.py
@@ -3,9 +3,9 @@ from papers import views
 
 urlpatterns = [
     # 展示主界面
-    path('papers/show_list/<pindex>', views.index, name='index'),
+    path('papers', views.index, name='index'),
     # 论文单独展示界面
     path('papers/pp<int:paper_id>/', views.detail, name='detail'),
     # 搜索结果界面
-    path('search/', views.search, name='search')
+    path('search/?s=<searchword>/page=<pindex>', views.search, name='search')
 ]
--- papers/views.py
+++ papers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.core.paginator import Paginator
 from django.db.models import Q
+import random
 
 from .models import PaperInfo, ConferenceInfo
 
@@ -9,7 +10,7 @@ from .models import PaperInfo, ConferenceInfo
 
 
 class MyPaginator(Paginator):   # 继承Paginator
-    def __init__(self, object_list, per_page, show_count=3, orphans=0, allow_empty_first_page=True):  # show_count代表要展示的当前页之前或之后的页码数，默认展示3页
+    def __init__(self, object_list, per_page, show_count=2, orphans=0, allow_empty_first_page=True):  # show_count代表要展示的当前页之前或之后的页码数，默认展示3页
         super().__init__(object_list, per_page, orphans, allow_empty_first_page)  # 继承父类的属性和方法
         self.show_count = show_count
         self.has_previous_more = True  # 定义show_count之前是否还有更多页码
@@ -35,15 +36,11 @@ class MyPaginator(Paginator):   # 继承Paginator
 
 # Create your views here.
 # 论文首页，应该是论文列表加上搜索框
-def index(request, pindex):
+def index(request):
     papers_list = PaperInfo.objects.all()
-    paginator = MyPaginator(papers_list, 10)
-    if pindex == "":  # django中默认返回空值，所以加以判断，并设置默认值为1
-        pindex = 1
-    else:
-        int(pindex)
-    page = paginator.page(pindex)
-    context = {"page": page, "paginator": paginator}
+    # 随机取五份论文，之后会修改为根据个人兴趣推荐
+    papers = random.sample(list(papers_list), 5)
+    context = {"papers": papers}
     return render(request, 'papers/index.html', context)
 
 # 论文详情页
@@ -57,20 +54,24 @@ def detail(request, paper_id):
 # 搜索页
 
 
-def search(request):
+def search(request, searchword, pindex):
     q = request.GET.get('q')
     error_msg = ''
+    if(searchword and q == None):
+        q = searchword
+    searchword = q
     papers_list = PaperInfo.objects.all()
     paginator = MyPaginator(papers_list, 10)
-    page = paginator.page(1)
-    context = {"page": page, "paginator": paginator}
+    int(pindex)
+    page = paginator.page(pindex)
+    context = {"page": page, "paginator": paginator, "searchword": searchword}
     if not q:
         error_msg = "请输入关键词"
         context['error_msg'] = error_msg
-        return render(request, 'papers/index.html', context)
+        return render(request, 'papers/result.html', context)
     papers_list = PaperInfo.objects.filter(Q(paper_title__icontains=q) | Q(abstract__icontains=q))
     # papers_list = PaperInfo.objects.filter(Q(paper_title__icontains=q) | Q(abstract__icontains=q)) # 这是在摘要中查询
     paginator = MyPaginator(papers_list, 10)
-    page = paginator.page(1)
-    context = {"page": page, "paginator": paginator, "error_msg": error_msg}
-    return render(request, 'papers/index.html', context)
+    page = paginator.page(pindex)
+    context = {"page": page, "paginator": paginator, "error_msg": error_msg,  "searchword": searchword}
+    return render(request, 'papers/result.html', context)
