689b263304cffa6f248024d3f290ec6f90d6915b
Mizuhara-yaguchi <380665279@qq.com>
2020-10-21 00:21:51 +0800

    branch

--- prediction/urls.py
+++ prediction/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from prediction import views
 
-urlpatterns = [ 
+from prediction import views  #添加的
+
+urlpatterns = [
+    # 展示主界面
+    path('prediction/', views.index, name='index'),
+    path('prediction/result/', views.result, name='result'),
 ]
