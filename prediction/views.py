2115a857f55743fe175e1a73e69c71cefdd98c3e
Mizuhara-yaguchi <380665279@qq.com>
2020-10-20 16:22:58 +0800

    论文提交表单

--- prediction/views.py
+++ prediction/views.py
@@ -5,4 +5,9 @@ def index(request):
     return render(request, 'prediction/index.html')
 
 def result(request):
+    if request.method == 'POST':
+        name = request.POST.get('title')
+        content = request.POST.get('content')
+        author = request.POST.get('author')
+    print(name, content, author)
     return render(request, 'prediction/result.html')
