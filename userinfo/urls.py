58d4b82478a5ff468faf5f4127a974f62f076952
Herixth <17361019875@163.com>
2020-10-20 16:38:46 +0800

    登录信息提醒

--- userinfo/urls.py
+++ userinfo/urls.py
@@ -1,4 +1,4 @@
-from userinfo.inbox.views import inbox_overview
+from userinfo.inbox.views import inbox_check_all_unread, inbox_detail, inbox_overview
 from userinfo.views import profile_view
 from django.urls import path, include
 from userinfo import views as user_views
@@ -13,6 +13,8 @@ urlpatterns = [
     path('confirm/', user_views.user_confirm, name="confirm"),
     path('profile/<str:uuid>/', user_views.profile_view, name="profile"),
     path('inbox/', inbox_views.inbox_overview, name="inbox"),
+    path('inbox/<int:id>/', inbox_views.inbox_detail, name="inbox_detail"),
+    path('inbox/check_all_unread/', inbox_views.inbox_check_all_unread, name="check_all_unread"),
     path('captcha/', include('captcha.urls')),
     path('refresh_captcha/', captcha_views.refresh_captcha),
 ]
