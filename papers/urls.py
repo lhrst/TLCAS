5dee7455b068d3ba7d3830ecec392f9b0887c85d
CarOL <32268783+hx-w@users.noreply.github.com>
2021-03-12 19:26:01 +0800

    update url.py

--- papers/urls.py
+++ papers/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     # 论文单独展示界面
     path('papers/pp<int:paper_id>/', views.detail, name='detail'),
     # 收藏论文
-    path('papers/pp<int:paper_id>/collect', views.collect, name='collect'),
+    path('papers/pp<int:paper_id>/collect/', views.collect, name='collect'),
     # 取消收藏论文
-    path('papers/pp<int:paper_id>/cancel_collect', views.cancel_collect, name='cancel_collect'),
+    path('papers/pp<int:paper_id>/cancel_collect/', views.cancel_collect, name='cancel_collect'),
     # 搜索结果界面
     path('search/?s=<searchword>/page=<pindex>/', views.search, name='search')
 ]
