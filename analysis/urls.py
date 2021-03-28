23bf970cf34a5ee34f2d32bd022e3c0031d346b6
Mizuhara-yaguchi <380665279@qq.com>
2021-03-28 17:36:13 +0800

    analysis

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
-from analysis import views
 
-urlpatterns = [ 
+from analysis import views  #添加的
+
+urlpatterns = [
+    # 展示主界面
+    path('analysis/', views.index, name='index'),
+    path('analysis/AAAI/', views.AAAI, name='AAAI'),
+    path('analysis/ACL/', views.ACL, name='ACL'),
+    path('analysis/CVPR/', views.CVPR, name='CVPR'),
+    path('analysis/ICML/', views.ICML, name='ICML'),
+    path('analysis/ICCV/', views.ICCV, name='ICCV'),
+    path('analysis/NIPS/', views.NIPS, name='NIPS'),
+    path('analysis/IJCAI/', views.IJCAI, name='IJCAI'),
 ]
