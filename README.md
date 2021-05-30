b122a09345f03c9a2988dd0f0ae653969092995a
zhfffy <564952043@qq.com>
2021-05-30 14:49:58 +0800

    5.30更新

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TLCAS
+# 学术顶级会议分析系统
 top_level_conference_analysis_system
 这是一个AI领域七个顶级会议(CCF-A)的论文分析系统，我们希望通过这个系统，能找到该领域的学术研究趋势，并帮助研究者更敏锐地发现新的学术热点。
 
@@ -8,6 +8,7 @@ tasklist
 - [x] 获取各会议论文的标题，摘要，作者并制作成数据集。
 - [ ] 开始制作网站，使用django作为后端开发框架
   - [x] 建立models
+<<<<<<< Updated upstream
   - [ ] 将数据集批量导入models中
 - [ ] 对数据集进行分析，尝试得出一些结论
 
@@ -18,3 +19,39 @@ tasklist
 3. `python3 manage.py collectstatic`
 4. `python3 manage.py migrate`
 5. `python3 manage.py runserver`
+=======
+  - [x] 将数据集批量导入models中
+- [x] 首页，搜索页，详情页
+- [x] 用户资料页
+- [x] 用户收藏论文，最近观看论文
+- [x] 论文推荐系统
+
+
+## BUILD
+
+1. `pip install -r requirements`
+2. `python manage.py collectstatic`
+3. `python manage.py migrate`
+4. `python manage.py runserver`
+
+
+
+## UPDATE
+
+1. 所有的`base.html/nav.html/footer.html`以userinfo/templates/下的为准
+
+2. 修复部分papers下的bug
+
+3. 提供站内通知功能
+
+   ```python
+   from userinfo.inbox import views as inbox_views
+   
+   ### 在合适的地方调用即可发送站内通知
+   ### user: UserInfomation的对象实例，可以是request.user
+   ### title, content都是str
+   inbox_views.send_inbox(user, title, content) 
+   ```
+
+   
+>>>>>>> Stashed changes
--- analysis/views.py
+++ analysis/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
 
+<<<<<<< Updated upstream
 # Create your views here.
+=======
+def index(request):
+    return render(request, 'analysis/index.html')
+
+def AAAI(request):
+    return render(request, 'analysis/AAAI.html')
+def AAAI1(request):
+    return render(request, 'analysis/AAAI1.html')
+def AAAI2(request):
+    return render(request, 'analysis/AAAI2.html')
+
+def ACL(request):
+    return render(request, 'analysis/ACL.html')
+def ACL1(request):
+    return render(request, 'analysis/ACL1.html')
+def ACL2(request):
+    return render(request, 'analysis/ACL2.html')
+
+def CVPR(request):
+    return render(request, 'analysis/CVPR.html')
+def CVPR1(request):
+    return render(request, 'analysis/CVPR1.html')
+def CVPR2(request):
+    return render(request, 'analysis/CVPR2.html')
+
+def ICML(request):
+    return render(request, 'analysis/ICML.html')
+def ICML1(request):
+    return render(request, 'analysis/ICML1.html')
+def ICML2(request):
+    return render(request, 'analysis/ICML2.html')
+
+def ICCV(request):
+    return render(request, 'analysis/ICCV.html')
+def ICCV1(request):
+    return render(request, 'analysis/ICCV1.html')
+def ICCV2(request):
+    return render(request, 'analysis/ICCV2.html')
+
+def NIPS(request):
+    return render(request, 'analysis/NIPS.html')
+def NIPS1(request):
+    return render(request, 'analysis/NIPS1.html')
+def NIPS2(request):
+    return render(request, 'analysis/NIPS2.html')
+
+def IJCAI(request):
+    return render(request, 'analysis/IJCAI.html')
+def IJCAI1(request):
+    return render(request, 'analysis/IJCAI1.html')
+def IJCAI2(request):
+    return render(request, 'analysis/IJCAI2.html')
+>>>>>>> Stashed changes
